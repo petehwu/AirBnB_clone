a7d2a80a3aca1655edf2444cba8b0d900e05a5e3
byn3 <bryanleung92@gmail.com>
2018-11-12 17:39:18 +0000

    Add: New classes that will relay info about a user's establishment, hosting or reviews

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A User class that inherits from BaseModel """
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ Inherits from BaseModel,
+    instantiate with empty string as values for attributes """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
