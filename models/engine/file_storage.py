42f7e9617f83b26afb64fe9835e9ebde0f78c04b
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-11-13 10:49:20 -0800

    changed file_storage so it works with empty file.json file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,6 +39,7 @@ class FileStorage:
                     "Review": Review}
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, mode='r', encoding='utf-8') as f:
-                x = json.loads(f.read())
-                for k, v in x.items():
-                    self.__objects[k] = obj_dict[v["__class__"]](**v)
+                if f.read():
+                    x = json.loads(f.read())
+                    for k, v in x.items():
+                        self.__objects[k] = obj_dict[v["__class__"]](**v)
