fa631afc6a300496fc1de9f34590c1d74142069f
byn3 <bryanleung92@gmail.com>
2018-11-14 07:58:21 +0000

    Change: Added execute permissions to the files

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -16,7 +16,37 @@ from models.review import Review
 class TestFileStorage(unittest.TestCase):
     """test cases for FileStorageclass"""
 
-    def test_file_storage(self):
-        """testing the BaseModel"""
+    def setUp(self):
+        """ sets up this each test """
+        self.user = User()
+        self.user.id = "98"
+        self.user.first_name = "Peter"
+        self.user.last_name = "Wu"
+        self.user.email = "naruto@hokage.com"
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        """ tears down after each test. resets """
+        del self.user
+
+    def test_docstring(self):
+        """ checks if docstring is there """
+        self.assertIsNotNone(FileStorage.__doc__)
+
+    def test_file_storage_is_dict(self):
+        """testing if dict"""
         st = FileStorage()
-        st.reload()
+        st_dict = st.all()
+        self.assertTrue(st_dict)
+        self.assertEqual(dict, type(st_dict))
+
+    def test_peter_wu(self):
+        """ checks peter out. whatta hottie """
+        self.assertTrue(self.user)
+        self.assertEqual(self.user.first_name, "Peter")
+        self.assertFalse(self.user.password)
+        self.assertNotEqual(self.user.id, 98)
+        self.assertEqual(self.user.id, "98")
+
+if __name__ == "__main__":
+    unittest.main()
